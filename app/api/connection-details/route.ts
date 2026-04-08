96359e7486442f89c4f90f0e0979d83c475dd1b9
Roman Piacquadio <roman.piacquadio@cloudx.com>
2026-04-08 13:40:42 -0300

    agent name 'wil-local-agent'

--- app/api/connection-details/route.ts
+++ app/api/connection-details/route.ts
@@ -32,7 +32,7 @@ type ConversationContext = {
 const API_KEY = process.env.LIVEKIT_API_KEY;
 const API_SECRET = process.env.LIVEKIT_API_SECRET;
 const LIVEKIT_URL = process.env.LIVEKIT_URL;
-const HARDCODED_AGENT_NAME = 'wil-prod-agent';
+const HARDCODED_AGENT_NAME = 'wil-local-agent';
 //const HARDCODED_AGENT_NAME = '';
 
 export const revalidate = 0;
